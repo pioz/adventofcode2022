227a55f5afd74ce9f6aebd3bbefb82340906f646
pioz <epilotto@gmx.com>
2022-12-02 15:37:19 +0100

    Use array index rotation

--- 2/part2.go
+++ 2/part2.go
@@ -6,24 +6,17 @@ import (
 	"os"
 )
 
-var scores = []int{1, 2, 3}
+var scores = [3]int{1, 2, 3}
 
 func myScore(elfMove int, result int) int {
-	if result == 0 {
+	if result == 1 { // draw
 		return scores[elfMove] + 3
 	}
-	if result > 0 {
-		index := elfMove + 1
-		if index == 3 {
-			index = 0
-		}
-		return scores[index] + 6
+	if result == 2 { // win
+		return scores[(elfMove+1)%3] + 6 // rotate right
 	}
-	index := elfMove - 1
-	if index == -1 {
-		index = 2
-	}
-	return scores[index] + 0
+	// loss
+	return scores[(elfMove-1+3)%3] + 0 // rotate left
 }
 
 func main() {
@@ -33,12 +26,10 @@ func main() {
 	}
 	scanner := bufio.NewScanner(file)
 	score := 0
-	decryptElf := map[rune]int{'A': 0, 'B': 1, 'C': 2}
-	decryptResult := map[rune]int{'X': -1, 'Y': 0, 'Z': 1}
 	for scanner.Scan() {
 		s := scanner.Text()
-		elfMove := decryptElf[rune(s[0])]
-		result := decryptResult[rune(s[2])]
+		elfMove := int(s[0] - 65)
+		result := int(s[2] - 88)
 		score += myScore(elfMove, result)
 	}
 	fmt.Println(score)
